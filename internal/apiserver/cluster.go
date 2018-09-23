b89069fd59ad4850ced0a1fd00b796c9cfbd9bdb
Michael Venezia <mvenezia@gmail.com>
2018-09-22 22:12:31 -0400

    Test branch

--- internal/apiserver/cluster.go
+++ internal/apiserver/cluster.go
@@ -8,29 +8,37 @@ import (
 )
 
 func (s *Server) CreateCluster(ctx context.Context, in *pb.CreateClusterMsg) (*pb.CreateClusterReply, error) {
-	cluster := TranslateAPI(in)
-	err := ApplyManifests(cluster)
-	if err != nil {
-		// TODO: Make this consistent with how the CMA does logging...
-		fmt.Printf("ERROR: CreateCluster, name %v, err %v", in.Name, err)
-		return &pb.CreateClusterReply{
-			Ok: false,
-			Cluster: &pb.ClusterItem{
-				Id:     "stub",
-				Name:   in.Name,
-				Status: "CreateFailed",
-			},
-		}, nil
-	}
-
 	return &pb.CreateClusterReply{
 		Ok: true,
 		Cluster: &pb.ClusterItem{
-			Id:     "stub",
+			Id:     "created cluster",
 			Name:   in.Name,
-			Status: "Creating",
+			Status: "is OK",
 		},
 	}, nil
+	//cluster := TranslateAPI(in)
+	//err := ApplyManifests(cluster)
+	//if err != nil {
+	//	// TODO: Make this consistent with how the CMA does logging...
+	//	fmt.Printf("ERROR: CreateCluster, name %v, err %v", in.Name, err)
+	//	return &pb.CreateClusterReply{
+	//		Ok: false,
+	//		Cluster: &pb.ClusterItem{
+	//			Id:     "stub",
+	//			Name:   in.Name,
+	//			Status: "CreateFailed",
+	//		},
+	//	}, nil
+	//}
+	//
+	//return &pb.CreateClusterReply{
+	//	Ok: true,
+	//	Cluster: &pb.ClusterItem{
+	//		Id:     "stub",
+	//		Name:   in.Name,
+	//		Status: "Creating",
+	//	},
+	//}, nil
 }
 
 func (s *Server) GetCluster(ctx context.Context, in *pb.GetClusterMsg) (*pb.GetClusterReply, error) {
@@ -38,27 +46,45 @@ func (s *Server) GetCluster(ctx context.Context, in *pb.GetClusterMsg) (*pb.GetC
 		Ok: true,
 		Cluster: &pb.ClusterDetailItem{
 			Id:         "stub",
-			Name:       "stub",
+			Name:       in.Name,
 			Status:     "stub",
 			Kubeconfig: "xyz",
 		},
 	}, nil
+	//return &pb.GetClusterReply{
+	//	Ok: true,
+	//	Cluster: &pb.ClusterDetailItem{
+	//		Id:         "stub",
+	//		Name:       "stub",
+	//		Status:     "stub",
+	//		Kubeconfig: "xyz",
+	//	},
+	//}, nil
 }
 
 func (s *Server) DeleteCluster(ctx context.Context, in *pb.DeleteClusterMsg) (*pb.DeleteClusterReply, error) {
-	err := DeleteManifests(in.Name)
-	if err != nil {
-		return &pb.DeleteClusterReply{Ok: false, Status: "DeleteFailed"}, nil
-	}
-
-	return &pb.DeleteClusterReply{Ok: true, Status: "Deleted"}, nil
+	return &pb.DeleteClusterReply{Ok: true, Status: "Test Delete -->" + in.Name + "<--"}, nil
+	//err := DeleteManifests(in.Name)
+	//if err != nil {
+	//	return &pb.DeleteClusterReply{Ok: false, Status: "DeleteFailed"}, nil
+	//}
+	//
+	//return &pb.DeleteClusterReply{Ok: true, Status: "Deleted"}, nil
 }
 
 func (s *Server) GetClusterList(ctx context.Context, in *pb.GetClusterListMsg) (reply *pb.GetClusterListReply, err error) {
 	reply = &pb.GetClusterListReply{
 		Ok: true,
+		Clusters: []*pb.ClusterItem{
+			{Id: "test", Name: "test name", Status: "testStatus"},
+			{Id: "test2", Name: "test2 name", Status: "testStatus2"},
+		},
 	}
 	return reply, nil
+	//reply = &pb.GetClusterListReply{
+	//	Ok: true,
+	//}
+	//return reply, nil
 }
 
 func (s *Server) AdjustClusterNodes(ctx context.Context, in *pb.AdjustClusterMsg) (*pb.AdjustClusterReply, error) {
